82b195c7f5ec66b413572aac89378430b2fff812
yiyun <yiyungent@gmail.com>
2019-03-04 17:37:49 +0800

    状态模式初版

---
+++ 状态模式/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 状态模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Work emergencyProjects = new Work();
+            emergencyProjects.Hour = 9;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 10;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 12;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 13;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 14;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 17;
+
+            emergencyProjects.TaskFinished = false;
+
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 19;
+            emergencyProjects.WriteProgram();
+            emergencyProjects.Hour = 22;
+            emergencyProjects.WriteProgram();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 状态模式/Work.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 状态模式
+{
+    public class Work
+    {
+        private int hour;
+
+        public int Hour { get => hour; set => hour = value; }
+
+        private bool finish = false;
+        public bool TaskFinished
+        {
+            get { return finish; }
+            set { finish = value; }
+        }
+        public void WriteProgram()
+        {
+            if (hour < 12)
+            {
+                Console.WriteLine("当前时间：{0}点 上午工作，精神百倍", hour);
+            }
+            else if (hour < 13)
+            {
+                Console.WriteLine("当前时间 {0}点 饿了， 午饭：犯困，午休", hour);
+            }
+            else if (hour < 17)
+            {
+                Console.WriteLine("当前时间：{0}点 下午状态还不错，继续努力", hour);
+            }
+            else
+            {
+                if (hour < 21)
+                {
+                    Console.WriteLine("当前时间：{0}点 加班哦，疲累之极", hour);
+                }
+                else
+                {
+                    Console.WriteLine("当前时间：{0}点 不行了，睡着了", hour);
+                }
+            }
+        }
+    }
+}
